0c2e5a01bd9805a1a2f184a832ebd9b14ce1711f
Sarnendu Mukherjee <sarnendu.mukherjee@gmail.com>
2020-10-11 19:25:50 +0200

    updated url to use https instead of http

--- src/service/Services.ts
+++ src/service/Services.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GetSeasonDetailsResponse, GetSeasonsResponse } from "./Service.model";
 
 const URL_GET_SESSIONS_2005_TO_2015 =
-  "http://ergast.com/api/f1/driverstandings/1.json?limit=11&offset=55";
+  "https://ergast.com/api/f1/driverstandings/1.json?limit=11&offset=55";
 
 export const getSeasons = (): Promise<{ data: GetSeasonsResponse }> => {
   return axios.get(URL_GET_SESSIONS_2005_TO_2015);
@@ -11,5 +11,5 @@ export const getSeasons = (): Promise<{ data: GetSeasonsResponse }> => {
 export const getSeasonDetailsBySeason = (
   season: string
 ): Promise<{ data: GetSeasonDetailsResponse }> => {
-  return axios.get(`http://ergast.com/api/f1/${season}/results/1.json`);
+  return axios.get(`https://ergast.com/api/f1/${season}/results/1.json`);
 };
